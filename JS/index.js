eb8b61c250eb1e1c093de1c49f2ad973d79a430e
Ashutosh1499 <akrobicsos14@gmail.com>
2023-03-16 17:36:49 +0530

    Mobile compatibility - 3

--- JS/index.js
+++ JS/index.js
@@ -290,3 +290,18 @@ scrollToContact = () => {
 	let elem = document.getElementById('contactSection');
 	elem.scrollIntoView({ behavior: 'smooth' });
 };
+let mobileAlert = document.getElementById('mobileAlert');
+
+if (
+	navigator.userAgent.match(/Android/i) ||
+	navigator.userAgent.match(/webOS/i) ||
+	navigator.userAgent.match(/iPhone/i) ||
+	navigator.userAgent.match(/iPad/i) ||
+	navigator.userAgent.match(/iPod/i) ||
+	navigator.userAgent.match(/BlackBerry/i) ||
+	navigator.userAgent.match(/Windows Phone/i)
+) {
+	f;
+	alert('Please switch to a desktop for better experience.');
+} else {
+}
